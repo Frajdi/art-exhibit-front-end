55a705ec21204ec4e8150631f79746b768ef8a1b
Frajdi <frajdimalaj@gmail.com>
2023-08-15 02:49:43 +0200

    set up routing

--- art-exhibit/src/main.jsx
+++ art-exhibit/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import withRoutes from './state/withRoutes.jsx'
+
+const RoutedApp = withRoutes(App)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RoutedApp />
   </React.StrictMode>,
 )
---
+++ art-exhibit/src/state/withRoutes.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LandingPage from "../containers/LandingPage";
+
+const withRoutes = (App) => (props) => {
+  const routes = createBrowserRouter([
+    {
+      path: "/",
+      element: <LandingPage />
+    }
+  ]);
+
+  return (
+    <RouterProvider router={routes}>
+      <App {...props} />
+    </RouterProvider>
+  );
+};
+
+export default withRoutes;
