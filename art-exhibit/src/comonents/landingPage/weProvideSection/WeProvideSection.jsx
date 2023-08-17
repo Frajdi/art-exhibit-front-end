21f4b42dddf38f7aa48f4de067cd59d5aa744612
Frajdi <frajdimalaj@gmail.com>
2023-08-17 18:19:56 +0200

    added provide section fully

--- art-exhibit/src/comonents/landingPage/weProvideSection/WeProvideSection.jsx
+++ art-exhibit/src/comonents/landingPage/weProvideSection/WeProvideSection.jsx
@@ -1,18 +1,61 @@
-import React from 'react'
-import Card from './Card'
-import Grid from '@mui/material/Grid'
-import ColorLensOutlinedIcon from '@mui/icons-material/ColorLensOutlined';
-import BrushOutlinedIcon from '@mui/icons-material/BrushOutlined';
+import React from "react";
+import Card from "./Card";
+import Grid from "@mui/material/Grid";
+import BrushOutlinedIcon from "@mui/icons-material/BrushOutlined";
+import DevicesOutlinedIcon from '@mui/icons-material/DevicesOutlined';
+import TranslateOutlinedIcon from '@mui/icons-material/TranslateOutlined';
+import MeetingRoomOutlinedIcon from '@mui/icons-material/MeetingRoomOutlined';
+import PowerOutlinedIcon from '@mui/icons-material/PowerOutlined';
+import ConstructionOutlinedIcon from '@mui/icons-material/ConstructionOutlined';
+
+const sectionData = [
+    {
+        title: "Artwork enquires",
+        subtitle: "Track website enquiries all in one place so you never miss an opportunity.",
+        icon: BrushOutlinedIcon
+    },
+    {
+        title: "Quick and responsive",
+        subtitle: "Our templates are automatically built to be fully responsive on all devices.",
+        icon: DevicesOutlinedIcon
+    },
+    {
+        title: "Multiple languages",
+        subtitle: "Increase your global reach with easy multi-language management.",
+        icon: TranslateOutlinedIcon
+    },
+    {
+        title: "Online Viewing Rooms",
+        subtitle: "Create time-sensitive online exhibitions and collect invaluable collector data.",
+        icon: MeetingRoomOutlinedIcon
+    },
+    {
+        title: "Platform integration",
+        subtitle: "Build pages, track enquiries and online sales, all within a few clicks.",
+        icon: PowerOutlinedIcon
+    },
+    {
+        title: "Advanced SEO tools",
+        subtitle: "Maximise your prominence among search results with powerful SEO tools.",
+        icon: ConstructionOutlinedIcon
+    },
+]
 
 const WeProvideSection = () => {
   return (
-    <Grid container>
-    <Grid item xs={4}>
-    <Card title={'Artwork enquires'} subtitle={'Track website enquiries all in one place so you never miss an opportunity.'} Icon={BrushOutlinedIcon}/>
-
+    <Grid spacing={10} container>
+    {sectionData.map(item => {
+        return <Grid item xs={4}>
+        <Card
+          title={item.title}
+          subtitle={item.subtitle}
+          Icon={item.icon}
+        />
+      </Grid>
+    })}
+      
     </Grid>
-    </Grid>
-  )
-}
+  );
+};
 
-export default WeProvideSection
+export default WeProvideSection;
