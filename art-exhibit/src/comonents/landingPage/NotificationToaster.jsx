cdb5b1b17e9248dbc8734ae4dc62be503dba9063
Frajdi <frajdimalaj@gmail.com>
2023-08-31 10:57:38 +0200

    FIXED NOTIFICATION TOEASTER

--- art-exhibit/src/comonents/landingPage/NotificationToaster.jsx
+++ art-exhibit/src/comonents/landingPage/NotificationToaster.jsx
@@ -22,9 +22,9 @@ const containerStyles = {
     backgroundColor: "rgba(245,233,255, 0.7)",
     backdropFilter: " blur( 15px )",
     border: '1px solid #C882FF',
-    position: 'absolute',
+    position: 'fixed',
     zIndex: "99999", // z-50
-    bottom: "16px", // bottom-4
+    top: "16px", // bottom-4
     right: "16px", // right-4,
 }
 
